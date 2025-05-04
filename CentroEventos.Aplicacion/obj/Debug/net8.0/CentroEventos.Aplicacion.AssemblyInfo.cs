87ee47bfab49ed96b19fa42b95184abbb18ffe52
Federico Jose Isla <federicoisla1@gmail.com>
2025-05-04 20:08:54 -0300

    movi la clase de vale de actividad

--- CentroEventos.Aplicacion/obj/Debug/net8.0/CentroEventos.Aplicacion.AssemblyInfo.cs
+++ CentroEventos.Aplicacion/obj/Debug/net8.0/CentroEventos.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31ad34a6ec1557673c0f9f80884297d9312dbb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6179bd60d82d5efecf455c99123b393f6590e981")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
