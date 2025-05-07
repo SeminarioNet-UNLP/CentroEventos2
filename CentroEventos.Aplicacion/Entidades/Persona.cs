146cec3f461127dfac45c796551aa8863b3edfe0
ValeBlanco <valblan29@gmail.com>
2025-05-07 15:48:45 -0300

    cambie cosas de persona

--- CentroEventos.Aplicacion/Entidades/Persona.cs
+++ CentroEventos.Aplicacion/Entidades/Persona.cs
@@ -1,28 +1,26 @@
 public abstract class Persona
+
+
 {
-    public Persona(int Id, int NumCarnet, string Nombre, string Apellido, string Direccion, string Facultad, string Telefono, string CorreoElectronico)
+    public Persona(int elId, int elDNI, string Nombre, string Apellido, string elEmail, string elTelefono)
     {
-     id = Id;
-     numCarnet= NumCarnet; 
+     id = elId;
+     DNI=elDNI;
      nombre =Nombre;
      apellido =Apellido;
-     direccion =Direccion;
-     facultad =Facultad;
-     telefono =Telefono;
-     correoElectronico =CorreoElectronico;
+     telefono =elTelefono;
+     email=elEmail;
     }
 
     public int id { get; set; }
-    public int numCarnet { get; set; }
+    public int DNI { get; set; }
     public string? nombre { get; set; }
     public string? apellido { get; set; }
-    public string? direccion { get; set; }
-    public string? facultad { get; set; }
+    public string? email { get; set; }
     public string? telefono {get; set;}
-    public string? correoElectronico { get; set; }
 
     public override string ToString()
     {
-        return $"{id} {numCarnet} {nombre} {apellido} {direccion} {facultad} {telefono} {correoElectronico}"; // hacer el to estring de una pesona
+        return $"{id} {DNI} {nombre} {apellido} {telefono} {email}"; // hacer el to estring de una pesona
     }
 }
