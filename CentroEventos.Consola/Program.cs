d1dd868c9e061958b44dfd3a595c9b5f2c6158df
pablocabe <pablocabe0@gmail.com>
2025-05-07 11:02:20 -0300

    Corregi detalles de sintaxis y agregue algun comentario

--- CentroEventos.Consola/Program.cs
+++ CentroEventos.Consola/Program.cs
@@ -1,6 +1,6 @@
 //test para cargar un estudiante
 using CentroEventos.Aplicacion;
-IPersonaRepositorio repositorio =new RepositorioEstudiante();
-EstudianteAltaCDU cduEstu = new(repositorio);
+IPersonaRepositorio repositorioE = new RepositorioEstudiante();
+EstudianteAltaCDU cduEstu = new(repositorioE);
 
 cduEstu.Ejecutar(new Estudiante(1,123,"federico","isla", "la plata","informatica","122345678","fede@",26078,"Lic.informatica" ));
