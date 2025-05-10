14fff0509011afbc3bc206f223f44f919ce873cd
Fefo7 <federicoisla1@gmail.com>
2025-05-10 15:04:30 -0300

    validaciones de reserva

--- CentroEventos.Aplicacion/Validaciones/ValidarReserva.cs
+++ CentroEventos.Aplicacion/Validaciones/ValidarReserva.cs
@@ -13,5 +13,41 @@ public class ValidarReserva
         _repoReserva = repoReserva;
 
     }
-
+    public bool ExistenPersonaEvento(int IdPersona, int IdEvento)
+    {
+        List<Persona> personas = _repoPersona.ListadoPersona();
+        List<EventoDeportivo> eventos = _repoEventoDeportivo.ListadoEventoDeportivo();
+        bool condiP= false;
+        bool condiE = false;
+        for (int i = 0; i < personas.Count() && !condiP ; i++)
+        {
+            if(personas[i].Id == IdPersona)
+            {   
+                condiP = true;
+            }
+        }
+        for (int i = 0; i < eventos.Count() && !condiE ; i++)
+        {
+            if(eventos[i].Id == IdEvento)
+            {   
+                condiE = true;
+            }
+        }
+        return (condiP && condiE);
+    }
+    public bool YaReservado(EventoDeportivo evento, Persona persona)
+    {
+        List<Reserva> reservas = _repoReserva.ListadoReserva();
+        if(reservas != null)
+        {
+            for (int i = 0; i < reservas.Count() ; i++)
+            {
+                if(reservas[i].PersonaId == persona.Id)
+                {
+                
+                }
+            }
+        }    
+        return true;
+    }
 }
