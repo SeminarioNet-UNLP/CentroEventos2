61603d832deda357844567f7fa38e45d50e067f8
ValeBlanco <valblan29@gmail.com>
2025-06-17 15:52:37 -0300

    hice casos de uso del usuario

---
+++ CentroEventos.Aplicacion/CasosDeUso/BajaUsuarioUseCase.cs
@@ -0,0 +1,52 @@
+using CentroEventos.Aplicaciones.Excepciones;
+using CentroEventos.Aplicaciones.Validaciones;
+using System.Collections.Generic;
+
+public class BajaUsuarioUseCase
+{
+    private readonly IRepositorioUsuario _repoUsuario;
+    private readonly IServicioAutorizacion _autorizador;
+
+    public BajaUsuarioUseCase(IRepositorioUsuario repoUsuario,
+                             IServicioAutorizacion autorizador)
+    {
+        _repoUsuario = repoUsuario;
+        _autorizador = autorizador;
+    }
+
+    public void Ejecutar(int idAEliminar, int idQueSolicita)
+    {
+        List<Usuario> usuarios = _repoUsuario.ListarUsuarios();
+
+        if (!_autorizador.PoseeElPermiso(idQueSolicita, Permiso.UsuarioBaja))
+        {
+            throw new FalloAutorizacionException();
+        }
+
+        bool existe = false;
+        if (usuarios != null)
+        {
+            for (int i = 0; i < usuarios.Count; i++)
+            {
+                if (usuarios[i].Id == idAEliminar)
+                {
+                    existe = true;
+                }
+            }
+        }
+
+        if (!existe)
+        {
+            throw new OperacionInvalidaException("El usuario a eliminar no existe.");
+        }
+
+        try
+        {
+            _repoUsuario.BajaUsuario(idAEliminar);
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
