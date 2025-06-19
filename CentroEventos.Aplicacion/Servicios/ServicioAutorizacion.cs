60cb8622ba2f2a515d807d92ca480956b647f14b
Federico Isla <f.isla@gedis.ar>
2025-06-19 02:38:34 -0300

    mas fixx

--- CentroEventos.Aplicacion/Servicios/ServicioAutorizacion.cs
+++ CentroEventos.Aplicacion/Servicios/ServicioAutorizacion.cs
@@ -1,25 +1,6 @@
 public class ServicioAutorizacion : IServicioAutorizacion
 {   
    
-    public Usuario? usuarioActivo { get; set; }
-    public bool LogIn(Usuario usuario)
-    {
-        usuarioActivo = usuario;
-        return true;
-    }    
-       
-
-    public bool LogOut()
-    {
-        usuarioActivo = null;
-        return true;
-    }
-
-    public Usuario? ObtenerUsuarioActivo()
-    {
-        return usuarioActivo;
-    }
-
     public bool PoseeElPermiso(Usuario usuario, Permiso permiso)
     {
         if (usuario != null)
