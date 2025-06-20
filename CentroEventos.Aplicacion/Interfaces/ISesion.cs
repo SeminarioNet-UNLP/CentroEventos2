848411aa01fe526f7d419d34c3a8ed4ebc9ad607
Federico Isla <f.isla@gedis.ar>
2025-06-19 21:25:40 -0300

    cambios de comentarios

--- CentroEventos.Aplicacion/Interfaces/ISesion.cs
+++ CentroEventos.Aplicacion/Interfaces/ISesion.cs
@@ -1,7 +1,7 @@
 public interface ISesion
 { 
     public bool LogIn(Usuario usuario);
-    //public bool Registrar(Usuario usuario); seria el alta de un usuario normal
+    
     public bool LogOut();
-    public Usuario? ObtenerUsuarioActivo(); // puede ser null por si no esta logeado nadie
+    public Usuario? ObtenerUsuarioActivo(); 
 }
